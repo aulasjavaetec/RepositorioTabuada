7dbbc8a07bca30f17724b5042fd142d8f4b68b51
aulasjavaetec <161389173+aulasjavaetec@users.noreply.github.com>
2024-02-26 21:45:56 -0300

    Segundo commit
    Adicionado código-fonte

---
+++ ProjetoLaco/src/ClassePrincipal.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ClassePrincipal {
+
+	public static void main(String[] args) {
+		
+		while (true){
+			System.out.println("Informe a calculadora:");
+			Scanner sc = new Scanner(System.in);
+			int numero = sc.nextInt();
+			
+			for(int i=0; i<=10; i++){
+				
+				System.out.printf(numero + " * " + i + " = ");
+				System.out.println(numero * i);
+	
+			}
+			
+			break;
+		}
+		
+
+	}
+
+}
